3ff1a1af5d18f7b6265da7792948c4a2e1ced9a4
Taiwo Ifedayo <taiwo.ifedayo.gabriel@gmail.com>
2024-09-01 22:59:55 +0000

    1 Sept 2024, 23:59

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -3,4 +3,13 @@ import 'package:hive/hive.dart';
 class TodoDatabase {
   List todoList = [];
   final myBox = Hive.openBox("mybox");
+  void createInitialData() {
+    todoList = [
+      ["Make Tutorial", false],
+      ["Do Excersice", false]
+    ];
+  }
+
+  void loadData() {}
+  void updateData() {}
 }
