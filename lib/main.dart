f2a516249b859e1319ccb23d3743366d502aae9f
Taiwo Ifedayo <taiwo.ifedayo.gabriel@gmail.com>
2024-09-01 22:54:13 +0000

    1 Sept 2024, 23:54

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todoapp/pages/home_page.dart';
 
-void main() {
+void main() async {
+  //init hive
+  await Hive.initFlutter();
+
+  var box = await Hive.openBox('myBox');
   runApp(const MyApp());
 }
 
